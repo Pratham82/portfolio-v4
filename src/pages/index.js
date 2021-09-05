4163381c5232c8af762940480e4f3692f1feb271
pratham82 <mali.prathamesh82@gmail.com>
2021-09-05 17:37:01 +0530

    Changed home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,14 +11,20 @@ const Heading = () => (
       <span role="img" aria-label="wave">
         👋{" "}
       </span>{" "}
-      I'm Prathamesh Mali, a Full Stack Developer
+      I'm Prathamesh
     </h2>
-    <h3>Living in Mumbai, IN</h3>
-    {/*Gatsby's link (this will work faster than normal link)*/}
-    Need a Developer{" "}
+    <h3>I'm a Full-stack Software Developer.</h3>
+    <h4>
+      Based in Mumbai{" "}
+      <span role="img" aria-label="pushpin">
+        {" "}
+        📍
+      </span>
+    </h4>
+    {/* Need a Developer{" "}
     <Link to="/contact" className="cleanLink">
       <b>Contact Me ↗</b>
-    </Link>
+    </Link> */}
   </Layout>
 )
 

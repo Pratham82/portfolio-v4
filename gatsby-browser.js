d71da8d6af51835d69f9bf7fa1637d813bc48a9b
pratham82 <mali.prathamesh82@gmail.com>
2021-09-05 17:36:38 +0530

    Added prismjs themes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,4 @@
-require("prismjs/themes/prism-tomorrow.css")
+// require("./src/styles/prism-themes/prism-material-oceanic.css")
+// require("./src/styles/prism-themes/prism-one-dark.css")
+// require("./src/styles/prism-themes/prism-shades-of-purple.css")
+require("./src/styles/prism-themes/prism-night-owl.css")

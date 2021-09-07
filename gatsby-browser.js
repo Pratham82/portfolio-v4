06e056ba78d5f97a23f9e0575ff8c6ae6265bf16
pratham82 <mali.prathamesh82@gmail.com>
2021-09-07 09:21:12 +0530

    Removed prism themes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,2 @@
 // require("./src/styles/prism-themes/prism-material-oceanic.css")
-// require("./src/styles/prism-themes/prism-one-dark.css")
-// require("./src/styles/prism-themes/prism-shades-of-purple.css")
 require("./src/styles/prism-themes/prism-night-owl.css")

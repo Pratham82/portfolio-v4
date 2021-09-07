1081d6911d68efddf817a2b7f0fc1dd9dfacb1f2
pratham82 <mali.prathamesh82@gmail.com>
2021-09-08 03:08:53 +0530

    Added project module

---
+++ src/data/projectData.js
@@ -0,0 +1,36 @@
+const projectData = [
+  {
+    title: "Do You Know Me",
+    desc: "CLI app",
+    git: "https://github.com/Pratham82/mark-1",
+    live: "https://replit.com/@pratham82/mark1?embed=true#index.js",
+    stackUsed: ["JS"],
+  },
+
+  {
+    title: "Star Wars Quiz",
+    desc: "CLI app",
+    imageKey: "",
+    git: "https://github.com/Pratham82/mark-2",
+    live: "https://replit.com/@pratham82/mark2?embed=true#index.js",
+    stackUsed: ["JS"],
+  },
+  {
+    title: "Minion translator",
+    desc: "App for translating english to Minion lang",
+    imageKey: "",
+    git: "https://github.com/Pratham82/minion-translator",
+    live: "https://minions-app-mark06.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS"],
+  },
+  {
+    title: "Yoda translator",
+    desc: "App for translating english to Yoda's language",
+    imageKey: "",
+    git: "https://github.com/Pratham82/yoda-translator",
+    live: "https://yoda-app-mark07.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS"],
+  },
+]
+
+export default projectData

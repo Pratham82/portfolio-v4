bbd1ab4a85aae47700fa26830ed16f719892cf35
pratham82 <mali.prathamesh82@gmail.com>
2021-09-12 02:02:03 +0530

    Added old projects data for backup

---
+++ src/data/oldProjectsData.js
@@ -0,0 +1,182 @@
+const neoGProjects = [
+  {
+    title: "Do You Know Me",
+    desc: "CLI app",
+    git: "https://github.com/Pratham82/mark-1",
+    live: "https://replit.com/@pratham82/mark1?embed=true#index.js",
+    stackUsed: ["JS", "CLI"],
+  },
+
+  {
+    title: "Star Wars Quiz",
+    desc: "CLI app",
+    imageKey: "",
+    git: "https://github.com/Pratham82/mark-2",
+    live: "https://replit.com/@pratham82/mark2?embed=true#index.js",
+    stackUsed: ["JS", "CLI"],
+  },
+  {
+    title: "Minion translator",
+    desc: "App for translating english to Minion lang",
+    imageKey: "",
+    git: "https://github.com/Pratham82/minion-translator",
+    live: "https://minions-app-mark06.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS"],
+  },
+  {
+    title: "Yoda translator",
+    desc: "App for translating english to Yoda's language",
+    imageKey: "",
+    git: "https://github.com/Pratham82/yoda-translator",
+    live: "https://yoda-app-mark07.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS"],
+  },
+  {
+    title: "Emoji Interpreter",
+    desc: "Animals emoji interpreter",
+    imageKey: "",
+    git: "https://github.com/Pratham82/emoji-interpreter",
+    live: "https://emoji-interpreter-mark08.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS", "REACT"],
+  },
+  {
+    title: "Book Recommnedation",
+    desc: "App for book recommendation list",
+    imageKey: "",
+    git: "https://github.com/Pratham82/book-recommendations",
+    live: "https://book-recommendation-mark09.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS", "REACT"],
+  },
+  {
+    title: "Cash Register",
+    desc: "Calculate change and denominations",
+    imageKey: "",
+    git: "https://github.com/Pratham82/cash-register-app",
+    live: "https://cash-register-mark10-pratham82.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS", "REACT"],
+  },
+]
+
+const fullStackAndReact = [
+  {
+    title: "LaunchPad",
+    desc: "Launchpad is a platform for sharing products",
+    imageKey: "",
+    git: "https://github.com/Pratham82/Launch_Pad",
+    live: "https://app-hunt-373e7.web.app/home",
+    stackUsed: ["React", "Ionic", "Firebase"],
+  },
+  {
+    title: "Contact manager",
+    desc: "Keep track of all your contacts",
+    imageKey: "",
+    git: "https://github.com/Pratham82/Contact-manager",
+    live: "https://contacts-manager-mern.herokuapp.com/",
+    stackUsed: ["React", "Context", "Nodejs", "Express", "MongoDB"],
+  },
+  {
+    title: "Issue Tracker",
+    desc: "Manage and track issues",
+    imageKey: "",
+    git: " https://github.com/Pratham82/Issue-tracker",
+    live: "",
+    stackUsed: ["React", "Redux", "HTML", "CSS", "JS"],
+  },
+  {
+    title: "React Movie Search",
+    desc: "Explore movies",
+    imageKey: "",
+    git: "https://github.com/Pratham82/cash-register-app",
+    live: "https://movie-search-react-2020.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS", "REACT", "TMDB API"],
+  },
+  {
+    title: "Github Wars",
+    desc: "Find Popular repos on github & battle with friends",
+    imageKey: "",
+    git: "https://github.com/Pratham82/Github-wars",
+    live: "https://github-war-app.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS", "REACT", "Github API"],
+  },
+  {
+    title: "Image gallery",
+    desc: "Find images using unsplash API",
+    imageKey: "",
+    git: "https://github.com/Pratham82/Image-Gallery-React-Tailwind",
+    live: "",
+    stackUsed: ["HTML", "CSS", "JS", "REACT", "Unsplash API"],
+  },
+]
+
+// const frontendReact = [
+//   {
+//     title: "React Movie Search",
+//     desc: "Explore movies",
+//     imageKey: "",
+//     git: "https://github.com/Pratham82/cash-register-app",
+//     live: "https://movie-search-react-2020.netlify.app/",
+//     stackUsed: ["HTML", "CSS", "JS", "REACT", "TMDB API"],
+//   },
+//   {
+//     title: "Github Wars",
+//     desc: "Find Popular repos on github & battle with friends",
+//     imageKey: "",
+//     git: "https://github.com/Pratham82/Github-wars",
+//     live: "https://github-war-app.netlify.app/",
+//     stackUsed: ["HTML", "CSS", "JS", "REACT", "Github API"],
+//   },
+//   {
+//     title: "Image gallery",
+//     desc: "Find images using unsplash API",
+//     imageKey: "",
+//     git: "https://github.com/Pratham82/Image-Gallery-React-Tailwind",
+//     live: "",
+//     stackUsed: ["HTML", "CSS", "JS", "REACT", "Unsplash API"],
+//   },
+// ]
+
+const frontEndVanillaJS = [
+  {
+    title: "PokeDex",
+    desc: "Find pokemon info",
+    imageKey: "",
+    git: "https://github.com/Pratham82/PokeDex",
+    live: "https://poke-app-vanilla-js.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS", "Poke API"],
+  },
+  {
+    title: "WeatherJS",
+    desc: "Find current weather info.",
+    imageKey: "",
+    git: "https://github.com/Pratham82/Weather-JS",
+    live: "https://weather-app-new.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS", "REACT", "OpenWeather API"],
+  },
+  {
+    title: "Github Finder",
+    desc: "Find github user info",
+    imageKey: "",
+    git: "https://github.com/Pratham82/Github-finder",
+    live: "https://github-finder-334.netlify.app/",
+    stackUsed: ["HTML", "CSS", "JS", "REACT", "Github API"],
+  },
+  {
+    title: "Vaishali Enterprises",
+    desc: "Landing page for vaishali enterprises",
+    imageKey: "",
+    git: "https://github.com/Pratham82/Vaishali-enterprises",
+    live: "https://vaishali-ent.herokuapp.com/",
+    stackUsed: ["HTML", "CSS", "Django"],
+  },
+]
+
+const backend = [
+  {
+    title: "CRUD API",
+    desc: "CRUD API built with typescript",
+    imageKey: "",
+    git: "https://github.com/Pratham82/typescript-express-mongo-crud",
+    // live: "",
+    stackUsed: ["TS", "Express", "Mongo", "Mongoose", "Docker"],
+  },
+]

8bbba19209e9aaed56a0eb2d95b7584796913664
pratham82 <mali.prathamesh82@gmail.com>
2021-09-12 02:01:43 +0530

    Added new projects navigation and its styles

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,27 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
-import projectData from "../data/projectData"
+import AllProjects from "../data/projectData"
 import Project from "../templates/project"
 import projectStyles from "../styles/project.module.scss"
 
-const Projects = () => (
-  <Layout>
-    <Head title="Projects" />
-    <h1>Projects</h1>
-    <div className={projectStyles.allProjects}>
-      {projectData.map(project => (
-        <Project
-          key={project.title}
-          title={project.title}
-          desc={project.desc}
-          stackUsed={project.stackUsed}
-          git={project.git}
-          live={project.live}
-        />
+const Projects = () => {
+  const [currentProject, setCurrentProject] = useState("neoG Projects")
+
+  return (
+    <Layout>
+      <Head title="Projects" />
+      <h1>Projects</h1>
+      {Object.entries(AllProjects).map(([type, data]) => (
+        <button
+          onClick={() => setCurrentProject(type)}
+          className={`${projectStyles.projectNav}
+            ${currentProject === type && projectStyles.activeProject}`}
+        >
+          {type}
+        </button>
       ))}
-    </div>
-  </Layout>
-)
+      <div className={projectStyles.allProjects}>
+        {AllProjects[currentProject].map(project => (
+          <Project
+            key={project.title}
+            title={project.title}
+            desc={project.desc}
+            stackUsed={project.stackUsed}
+            git={project.git}
+            live={project.live}
+          />
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export default Projects
